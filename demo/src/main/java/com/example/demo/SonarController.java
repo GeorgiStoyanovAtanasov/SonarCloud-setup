4bf16534954d974a16d62a2109fb354813ffdf91
first-branch <radeva7609@gmaill.com>
2024-06-24 11:14:29 +0300

    commit

---
+++ demo/src/main/java/com/example/demo/SonarController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SonarController {
+    @GetMapping("/")
+    public String message(){
+        return "hi";
+    }
+}
