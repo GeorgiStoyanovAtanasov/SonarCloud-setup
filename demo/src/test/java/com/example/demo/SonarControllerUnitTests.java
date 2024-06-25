d3b849550c73c43cb0c2127562584f8e60c10aea
first-branch <radeva7609@gmaill.com>
2024-06-25 10:26:52 +0300

    chj

--- demo/src/test/java/com/example/demo/SonarControllerUnitTests.java
+++ demo/src/test/java/com/example/demo/SonarControllerUnitTests.java
@@ -1,20 +1,29 @@
 package com.example.demo;
 
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
+
 import org.springframework.test.web.servlet.MockMvc;
+
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-@WebAppConfiguration
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+@ExtendWith(MockitoExtension.class)
+//@WebAppConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class SonarControllerUnitTests {
-
+    @InjectMocks
+    private SonarController sonarController =  new SonarController();
     @Autowired
     private MockMvc mockMvc;
 
@@ -23,5 +32,9 @@ class SonarControllerUnitTests {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hi"));
+    }
+    @Test
+    void testMessageEndpoint() {
+        assertThat(sonarController.message()).isEqualTo("hi");
     }
 }
